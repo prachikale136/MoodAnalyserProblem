08822b836a69d34a7145a535faf0135d4c77675a
Prachi <prachikale136@gmail.com>
2023-03-21 21:38:17 +0530

    [Prachi] Add: Ability to analyze and rerspond Happy or Sad mood

---
+++ MoodAnalyserProblem/MoodAnalyserProblem/MoodAnalyser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    internal class MoodAnalyser
+    {
+        private string Message;
+         public MoodAnalyser(string message)
+        {
+             this.Message = message;
+        }
+
+        public string MoodAnalyze()
+        {
+            if (this.Message.Contains("Sad"))
+                return "Sad";
+            else
+                return "Happy";
+        }
+    }
+}
